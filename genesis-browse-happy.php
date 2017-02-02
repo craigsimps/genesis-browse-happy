22305427c7a78750f6906829963e092b3e3b2cf9
Craig Simpson <craig@craigsimpson.scot>
2017-02-02 11:27:11 +0000

    Set version of IE to  and other minor updates

--- genesis-browse-happy.php
+++ genesis-browse-happy.php
@@ -3,15 +3,16 @@
  * Genesis Browse Happy
  *
  * @package    Genesis_Browse_Happy
- * @author     Craig Simpson <csimps@gmail.com>
+ * @author     Craig Simpson <craig@craigsimpson.scot>
  * @license    GPL-2.0+
  *
  * @wordpress-plugin
  * Plugin Name:       Genesis Browse Happy
  * Plugin URI:        https://github.com/craigsimps/genesis-browse-happy
- * Description:       Adds a basic notification bar and link to http://browsehappy.com if visitor is using IE8 or below.
- * Version:           1.0.0
+ * Description:       Places a notification at the top of the screen and links the user to <a href="http://browsehappy.com/">Browse Happy</a> if they are  using IE9 or below.
+ * Version:           1.0.1
  * Author:            Craig Simpson
+ * Author URI:		  https://craigsimpson.scot/
  * Text Domain:	      genesis-browse-happy
  * Domain Path	      /languages
  * License:           GPL-2.0+
@@ -20,32 +21,35 @@
  * GitHub Plugin URI: https://github.com/craigsimps/genesis-browse-happy
  */
 
+namespace BrowseHappy;
+
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-add_action( 'genesis_before', 'bh_browse_happy' );
+add_action( 'genesis_before', __NAMESPACE__ . '\\add_notice' );
 /**
- * Output the Browse Happy notice if browser is IE8 or below.
+ * Output the Browse Happy notice if browser is IE9 or below.
  *
  * @since 1.0.0
  */
-function bh_browse_happy() {
-	?>
-	<!--[if lte IE 8]>
-	<?php echo sprintf( '<p class="browser-upgrade">%s<strong>%s</strong>%s<a href="%s">%s</a>%s</p>', esc_html( 'You are using an ', 'genesis-browse-happy' ), esc_html( 'outdated', 'genesis-browse-happy' ), esc_html( ' browser. Please ', 'genesis-browse-happy' ), esc_url( 'http://browsehappy.com' ),	esc_html( 'upgrade your browser', 'genesis-browse-happy' ), esc_html( ' to improve your experience.', 'genesis-browse-happy' ) ); ?>
-	<![endif]-->
-	<?php
+function add_notice() {
+	echo '<!--[if lte IE 9]>';
+	printf(
+		/* translators: %s link to Browse Happy website */
+		wp_kses_post( __( '<p class="browser-upgrade">You are using an <strong>outdated</strong> browser. Please <a href="%s">upgrade your browser</a> to improve your experience and security.</p>', 'genesis-browse-happy' ) ),
+		esc_url( 'http://browsehappy.com/' )
+	);
+	echo '<![endif]-->';
 }
 
-add_action( 'wp_enqueue_scripts', 'bh_enqueue_style' );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_style' );
 /**
- * Include the Browse Happy styles if the browser is IE8 or below.
+ * Include the Browse Happy styles if the browser is IE9 or below.
  *
  * @since 1.0.0
  */
-function bh_enqueue_style() {
-	
+function enqueue_style() {
 	wp_enqueue_style( 'genesis-browse-happy', esc_url( plugin_dir_url( __FILE__ ) . 'css/genesis-browse-happy.css' ) );
 	wp_style_add_data( 'genesis-browse-happy', 'conditional', 'lte IE 9' );
 }
